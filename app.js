6ebf02fc1873adde1eacb14a621848cbf02b94b5
Samuel Obeng <samuelobeng2006@gmail.com>
2020-06-16 03:34:12 +0200

    Database integration

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const _ = require('lodash');
+const mongoose = require("mongoose");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -16,10 +17,23 @@ app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
-let posts = [];
+//connection to database called dbBlog
+mongoose.connect("mongodb://localhost:27017/blogDB", { useUnifiedTopology: true, useNewUrlParser: true});
+
+//schema for our database table
+const postSchema = {
+  title: String,
+  content: String
+};
+
+//create model from the schema
+const Post = mongoose.model("Post", postSchema);
 
 app.get("/", function(req,res){
-  res.render("home", {startingContent:homeStartingContent, posts:posts});
+  //find posts and render onto homepage
+  Post.find({}, function(error, posts){
+    res.render("home", {startingContent:homeStartingContent, posts:posts});
+  });
 
 });
 
@@ -36,24 +50,24 @@ app.get("/compose", function(req,res){
 });
 
 app.post("/compose",function(req,res){
-  let blogHeader = req.body.blogHeader;
-  let postContent = req.body.postBody;
-  let blogPost = {
-    postTitle: blogHeader,
-    content:postContent
-  }
-  posts.push(blogPost);
-  res.redirect("/");
+  //document that contains the post in the db
+  const post = new Post ({
+     title: req.body.postTitle,
+    content: req.body.postBody
+  });
+  post.save(function(error){
+    if(!error){
+      res.redirect("/");
+    }
+  });
 });
 
 
-app.get("/posts/:postName", function(req, res) {
-  let requestedTitle = _.lowerCase(req.params.postName); 
-  posts.forEach(function(post){
-    let storedTitle = _.lowerCase(post.postTitle);
-    if(storedTitle === requestedTitle){
-      res.render("post",{title:post.postTitle, content:post.content});
-    }
+app.get("/posts/:postId", function(req, res) {
+  //find and display post based on id
+  const requestedPostId = req.params.postId;
+  Post.findOne({_id:requestedPostId}, function(error, post){
+    res.render("post",{title:post.title, content:post.content});
   });
   
 });
